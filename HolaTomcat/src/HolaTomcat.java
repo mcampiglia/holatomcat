8dccf0465ac87807d843f620e9ec0de90f96ef9b
Mauricio Campiglia <mauricio@campiglia.org>
2015-09-13 22:46:12 -0300

    Carga inicial del proyecto.

---
+++ HolaTomcat/src/HolaTomcat.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HolaTomcat
+ */
+@WebServlet("/HolaTomcat")
+public class HolaTomcat extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaTomcat() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("");
+		pw.println("");
+		pw.println("");
+		pw.println("<h1>Hello World</h1>");
+		pw.println("");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
